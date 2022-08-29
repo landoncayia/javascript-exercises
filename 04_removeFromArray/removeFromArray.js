6ab912b59f0d881a6659fd3f47a9e8a64c70feb4
Landon Cayia <landon.cayia@gmail.com>
2022-08-29 17:40:49 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removals) {
+  let index;
+  for (number of removals) {
+    index = array.indexOf(number);
+    if (index !== -1) {
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
