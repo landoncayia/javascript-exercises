8224df6240100cf36c2a399d808f1a35477245f3
Landon Cayia <landon.cayia@gmail.com>
2023-03-25 16:12:22 -0400

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.length
+    ? nums.reduce((product, num) => product * num)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  const nums = Array.from({length: n}, (_, i) => i + 1);
+  return nums.length ? multiply(nums) : 1;
 };
 
 // Do not edit below this line
