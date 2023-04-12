bc03e80095b4ed5b64af47283c6c2f483d3b4ac9
Landon Cayia <landon.cayia@gmail.com>
2023-04-12 18:00:01 -0400

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+    return oldestAge < personAge ? person : oldest;
+  })
 };
+
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
